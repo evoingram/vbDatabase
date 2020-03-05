5361b9e42d9a7dc6e8536698ddd0838bcb7ecc3e
Erica L. Ingram <inquiries@aquoco.co>
2020-03-04 17:22:32 -0800

    added more to server.js

--- webapp/backend/api/server.js
+++ webapp/backend/api/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.send('<h1>🚀</h1>');
+});
 
 module.exports = server;
