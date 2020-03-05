bf4c64361a9a6980dd501d911941880867420a8e
Erica L. Ingram <inquiries@aquoco.co>
2020-03-04 17:23:18 -0800

    added logger middleware

--- webapp/backend/api/server.js
+++ webapp/backend/api/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const logger = require('../middleware/logger');
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
 	res.send('<h1>🚀</h1>');
